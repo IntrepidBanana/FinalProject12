041ba8bced5c74656b3c58577adf355efb35447f
Gimpel <073185076@EDU.YRDSB.NET>
2018-12-14 15:03:40 -0500

    Norway

---
+++ src/Character.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Character {
+	private int health;
+	private int damage;
+	private double speed;
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
+	public int getDamage() {
+		return damage;
+	}
+	
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+}
