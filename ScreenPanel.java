8e6ade87058b279342e596fc55af03bc18a5b772
IntrepidBanana <17112277+IntrepidBanana@users.noreply.github.com>
2018-12-14 00:42:40 -0500

    Add files via upload

--- ScreenPanel.java
+++ ScreenPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,6 +20,7 @@ public class ScreenPanel extends JPanel implements KeyListener {
 	double dy;
 	boolean keys[] = new boolean[4];
 	int timePressed[] = new int[4];
+
 	public ScreenPanel(Camera camera, IOHandler io) {
 		this.camera = camera;
 		this.io = io;
@@ -26,13 +28,13 @@ public class ScreenPanel extends JPanel implements KeyListener {
 		dx = camera.x;
 		dy = camera.y;
 	}
-	
-	
-	
-	
+
 	double camSpeed = 1;
+
+	//TEMP THINGY TO SIMULATE MOVEMENT
 	private void update() {
-		double nudge =0.5;
+		double nudge = 1;
+		System.out.println(nudge);
 		if (keys[0]) {
 			dy -= nudge;
 		}
@@ -45,30 +47,33 @@ public class ScreenPanel extends JPanel implements KeyListener {
 		if (keys[3]) {
 			dx += nudge;
 		}
-		cameraSmooth();
 	}
 
-	private void cameraSmooth(){
-		double deltaX = dx-camera.x;
-		double deltaY = dy-camera.y;
-//		System.out.println(deltaX + " " + deltaY);
-		int maxDist = 200;
+	private void cameraSmooth(double x, double y) {
+		double deltaX = x - camera.x;
+		double deltaY = y - camera.y;
+
+		double moveX = deltaX * (1 / (100-camSpeed*camSpeed));
+		double moveY = deltaY * (1 / (100-camSpeed*camSpeed));
+		if(Math.abs(moveX) < 0.05) {
+			moveX = 0;
+		}
+		if(Math.abs(moveY) < 0.05) {
+			moveY = 0;
+		}
+		double distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+
 		
-		double moveX = deltaX*(camSpeed/100);
-		double moveY = deltaY*(camSpeed/100);
-		
-		
-		System.out.println(deltaX + " " + deltaY);
-		camera.x+=moveX;
-		camera.y+=moveY;
+		camera.x += moveX;
+		camera.y += moveY;
 	}
-	
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 
 		super.paintComponent(g);
 		update();
+		cameraSmooth(dx, dy);
 		Graphics2D g2d = (Graphics2D) g;
 		for (Tile t : wm.tiles) {
 			g2d.setColor(t.color);
@@ -76,7 +81,8 @@ public class ScreenPanel extends JPanel implements KeyListener {
 			g2d.draw(l);
 		}
 
-		Shape point = new Rectangle2D.Double(dx+camera.getSize()/2-camera.x, dy+camera.getSize()/2-camera.y,5.0,5.0);
+		Shape point = new Rectangle2D.Double(dx + camera.getSize() / 2 - camera.x, dy + camera.getSize() / 2 - camera.y,
+				5.0, 5.0);
 		g2d.draw(point);
 		repaint();
 	}
@@ -114,6 +120,10 @@ public class ScreenPanel extends JPanel implements KeyListener {
 			break;
 		case KeyEvent.VK_D:
 			keys[3] = false;
+			break;
+		case KeyEvent.VK_SPACE:
+			dx+= new Random().nextInt(1000)-500;
+			dy+= new Random().nextInt(1000)-500;
 			break;
 		}
 	}
