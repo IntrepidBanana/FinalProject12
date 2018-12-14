9057e6a54cb2d907053aa7c3f9a609b5f757099c
Petlah <072906118@EDU.YRDSB.NET>
2018-12-14 14:42:37 -0500

    Some more stuff

--- Camera.java
+++ Camera.java
@@ -1,19 +1,19 @@
 
 public class Camera {
-	
-	double x = 0;
-	double y = 0;
-	private int size = 720;
-	
-	public Camera(int size) {
-		this.size=size;
-	}
-	
-	public int getSize() {
-		return size;
-	}
-	
-	
-	
-	
+ 
+ double x = 0;
+ double y = 0;
+ private int size = 640;
+ 
+ public Camera(int size) {
+  this.size=size;
+ }
+ 
+ public int getSize() {
+  return size;
+ }
+ 
+ 
+ 
+ 
 }
--- IOHandler.java
+++ IOHandler.java
@@ -3,32 +3,32 @@ import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 public class IOHandler implements KeyListener {
-	Camera camera;
-	boolean pressed = false;
-	ArrayList<Integer> keys = new ArrayList<>();
+ Camera camera;
+ boolean pressed = false;
+ ArrayList<Integer> keys = new ArrayList<Integer>();
 
-	public IOHandler(Camera camera) {
-		this.camera = camera;
+ public IOHandler(Camera camera) {
+  this.camera = camera;
 
-	}
+ }
 
-	public ArrayList<Integer> getUpdate() {
-		return keys;
-	}
+ public ArrayList<Integer> getUpdate() {
+  return keys;
+ }
 
-	public void keyPressed(KeyEvent e) {
-		keys.add(e.getKeyCode());
-	}
+ public void keyPressed(KeyEvent e) {
+  keys.add(e.getKeyCode());
+ }
 
-	public void keyReleased(KeyEvent e) {
-		if (keys.contains(e.getKeyCode())) {
-			keys.remove(e.getKeyCode());
-		}
-	}
+ public void keyReleased(KeyEvent e) {
+  if (keys.contains(e.getKeyCode())) {
+   keys.remove(e.getKeyCode());
+  }
+ }
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
+ @Override
+ public void keyTyped(KeyEvent e) {
+  // TODO Auto-generated method stub
 
-	}
+ }
 }
