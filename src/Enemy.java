130aa45f87ffcb2c15016caf366d23ac9b120876
Gimpel <073185076@EDU.YRDSB.NET>
2018-12-17 14:47:01 -0500

    Enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -4,5 +4,9 @@ public class Enemy extends Character {
 	public Enemy(int health, int strength, double speed) {
 		super(health, strength, speed);
 	}
+	
+	public void move(float moveDirection){
+		
+	}
 
 }
