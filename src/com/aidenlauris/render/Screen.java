35ac2c88f3f4fbde26fb2f37583eae0700bc2166
Petlah <072906118@EDU.YRDSB.NET>
2019-01-22 14:54:33 -0500

    -more comments

--- src/com/aidenlauris/render/Screen.java
+++ src/com/aidenlauris/render/Screen.java
@@ -11,6 +11,12 @@ import javax.swing.JFrame;
 import com.aidenlauris.game.GameLogic;
 import com.aidenlauris.game.util.IOHandler;
 
+/**
+ * @author Aiden & Lauris
+ * Jan 22, 2019
+ * 
+ * The physical screen where things are printed too
+ */
 public class Screen extends JFrame{
 	
 	IOHandler io = new IOHandler();
@@ -19,24 +25,42 @@ public class Screen extends JFrame{
 
 	
 	public Screen() {
+		
+		
+		//creates the window
 		setTitle("ScreenOverlay");
 		setSize(GameLogic.camx, GameLogic.camy);
+		
+		//close operation
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
 		getContentPane().setBackground(Color.BLUE);
+		
+		//add the render component
 		add(getPainter());
+		
+		//add all listeners
 		addKeyListener(io);
 		addMouseListener(io);
 		addMouseMotionListener(io);
 		setResizable(false);
+		
+		
 		setCursor( getToolkit().createCustomCursor(
                 new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB ),
                 new Point(),
                 null ) );
+		
+		//show it to the screen
 		setVisible(true);	
 		
 	}
 
 
+	/**
+	 * @return
+	 */
 	public RenderHandler getPainter() {
 		return painter;
 	}
